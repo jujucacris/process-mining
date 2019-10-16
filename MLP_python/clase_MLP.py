84a60af17a52bc88fcce6f588329c068257609db
arcardenasmaita <ar.cardenasmaita@gmail.com>
2019-10-15 22:18:59 -0300

    cMLP funcionando com XOR

--- MLP_python/clase_MLP.py
+++ MLP_python/clase_MLP.py
@@ -1,4 +1,4 @@
-import numpy as np
+﻿import numpy as np
 import math
 
 class cMLP(object):
@@ -19,18 +19,20 @@ class cMLP(object):
         Xtr=np.concatenate((np.ones((N,1),float),Xtr),axis=1)
         XVal=np.concatenate((np.ones((N_val,1),float),XVal),axis=1)
 
-        self.WA=np.random.randn(self.no,ne+1)/10 # mtrix pesos no x ne
-        self.WB=np.random.randn(ns,self.no+1)/10 # pesos
+        #self.WA= np.ones((self.no,ne+1),float) 
+        self.WA= np.random.randn(self.no,ne+1)/10 # matrix pesos no x ne
+        #self.WB= np.ones((ns,self.no+1),float) 
+        self.WB= np.random.randn(ns,self.no+1)/10 # pesos
 
         [Y,Z]=self.calc_saida(Xtr,self.WA,self.WB,N,self.funcao_f,self.funcao_g)
-        erro=Y-Ytr
+        erro=Y-Ytr #ekn
 
-        EQM=sum(sum(erro*erro))/N # erro total
-        nit=1
+        EQM=sum(sum(erro*erro))/N # sumatorio de erro quadratico medio
+        nit=1 # nro de epocas
 
         # Achar EQM em dados de validacao
         if(not(np.all(XVal==0))):
-            YVal_out=self.calc_saida(XVal,self.WA,self.WB,N_val,self.funcao_f,self.funcao_g)
+            [YVal_out, ZVal_out]=self.calc_saida(XVal,self.WA,self.WB,N_val,self.funcao_f,self.funcao_g)
             erro_val=YVal_out-YVal
             EQM_val=sum(sum(erro_val*erro_val))/N_val
             EQM_val_melhor=EQM_val  # a variable vai guardar o melhor EQM achado no conjunto de validacao
@@ -43,8 +45,8 @@ class cMLP(object):
         while(EQM>=1e-6 and nit<nitmax and nit_val<10000):
             nit = nit+1
             [gradA, gradB] = self.calc_grad(Xtr,Z,Y,erro,self.WB, N,self.funcao_f,self.funcao_g)
-            dirA=-gradA
-            dirB=-gradB
+            #dirA=-gradA
+            #dirB=-gradB
 
             self.WB=self.WB-alfa*gradB
             self.WA=self.WA-alfa*gradA
@@ -53,26 +55,26 @@ class cMLP(object):
             erro = Y-Ytr
 
             EQM = sum(sum(erro*erro))/N
-            vet_erro=np.concatenate((vet_erro,EQM),axis=1)
+            vet_erro=np.array([vet_erro,EQM])
 
             # validacao
-            if(not(np.all(XVal==0))):
-                YVal_out=self.calc_saida(XVal,self.WA,self.WB,N_val,self.funcao_f,self.funcao_g)
+            if(not(np.all(XVal==0))): # si el conj val no es vacio
+                [YVal_out,ZVal_out]=self.calc_saida(XVal,self.WA,self.WB,N_val,self.funcao_f,self.funcao_g)
                 erro_val=YVal_out-YVal
-                EQM_val=sum(sum(erro_val.dot(erro_val)))/N_val
-                vet_erro_val=np.concatenate((vet_erro_val, EQM_val),axis=1)
-            if( EQM_val < EQM_val_melhor):
-                nit_val=0
-                EQM_val_melhor=EQM_val  # a variable vai guardar o melhor EQM achado no conjunto de validacao
-                WA_melhor=self.WA #  WA_melhor vai guardar os melhores WA
-                WB_melhor=self.WB #  WA_melhor vai guardar os melhores WB
-                nit_melhor=nit
-            else:
-                nit_val=nit_val+1 # nit_val é o numero de iteracoes nas quais o EQM_val vai aumentando
+                EQM_val=sum(sum(erro_val*erro_val))/N_val
+                vet_erro_val=np.array([vet_erro_val, EQM_val])
+                if( EQM_val < EQM_val_melhor):
+                    nit_val=0
+                    EQM_val_melhor=EQM_val  # a variable vai guardar o melhor EQM achado no conjunto de validacao
+                    WA_melhor=self.WA #  WA_melhor vai guardar os melhores WA
+                    WB_melhor=self.WB #  WA_melhor vai guardar os melhores WB
+                    nit_melhor=nit
+                else:
+                    nit_val=nit_val+1 # nit_val é o numero de iteracoes nas quais o EQM_val vai aumentando
             if(not(np.all(XVal==0))):
                 self.WA=WA_melhor  # o objeto vai ficar com o melhor WA
                 self.WB=WB_melhor  # o objeto vai ficar com o melhor WB
-            return [Y,vet_erro,vet_erro_val,nit_melhor]
+        return [Y,vet_erro,vet_erro_val,nit_melhor]
 
     def testar_MLP(self,Xtest, Ytest):
         ne=Xtest.shape[1]
@@ -89,8 +91,8 @@ class cMLP(object):
         if funcao =='tan': # tangete hiperbolica
             output=np.tanh(input)  # fSaída entre 1 e -1
         elif funcao =='sig': # Sigmoide # Saída entre 0 e 1
-            output=1/(1+math.exp(-input))
-        elif funcao =='linear': # Linear
+            output=1/(1+np.exp(-input))
+        elif funcao =='linear': # Linear/identidAD
             output=input
         return output
 
@@ -109,10 +111,10 @@ class cMLP(object):
 
 
     def calc_saida(self,X,WA,WB,N,funcao_f,funcao_g):
-        Zin=X@(WA.conj().T) # obs: pode ser *?
+        Zin=X@(WA.T) 
         Z=self.ativacao(funcao_f,Zin)
         #Yin=[ones(N,1),Z]*WB';
-        Yin=np.concatenate((np.ones((N,1),float),Z),axis=1)@(WB.conj().T)
+        Yin=np.concatenate((np.ones((N,1),float),Z),axis=1)@(WB.T)
         Y=self.ativacao(funcao_g,Yin)
         return [Y,Z]
 
@@ -121,11 +123,11 @@ class cMLP(object):
         dg=self.derivada_funcao(funcao_g,Y)
 
         #1/N*(erro.*(dg))' *   [ones(N,1),Z];
-        grad_WB = 1/N@((erro*dg).conj().T)@(np.concatenate((np.ones((N,1),float), Z),axis=1))
+        grad_WB = 1/N*((erro*dg).T)@(np.concatenate((np.ones((N,1),float), Z),axis=1))
         #dJdZ = (erro.*(dg))*WB(:,2:end);
-        dJdZ = (erro*dg)@WB[1:]
+        dJdZ = (erro*dg)@WB[:,1:]
         #grad_WA = 1/N*(dJdZ.*(df))'*Xtr;
-        grad_WA = 1/N@((dJdZ*df).conj().T)@Xtr
+        grad_WA = 1/N*((dJdZ*df).T)@Xtr
         return grad_WA,grad_WB
 
 
--- MLP_python/main_simple.py
+++ MLP_python/main_simple.py
@@ -4,12 +4,12 @@ funcao_f='tan'
 funcao_g='sig'
 
 #Funcao AND
-Xtr=np.array([[1,0],[1,1],[0,0],[0,1]])
-Ytr=np.array([[0],[1],[0],[0]])
+#Xtr=np.array([[1,0],[1,1],[0,0],[0,1]])
+#Ytr=np.array([[0],[1],[0],[0]])
 
 #Funcao XOR
-# Xtr=[1 0;1 1;0 0; 0 1]
-# Ytr=[1;0;0; 1]
+Xtr=np.array([[1,0],[1,1],[0,0], [0,1]])
+Ytr=np.array([[1],[0],[0],[1]])
 
 Xtest=Xtr
 Ytest=Ytr
@@ -21,4 +21,3 @@ no=5
 oMLP = cMLP(funcao_f,funcao_g,no)
 [Yout_tr,vet_erro_tr,vet_erro_val,nit_parou]=oMLP.treinar_MLP(Xtr, Ytr,Xtest,Ytest,nitmax, alfa) #TODO add accuracy
 [Yout_test,EQM_test]=oMLP.testar_MLP(Xtest, Ytest)
-
