3f4a5177be00502d00047f0127dd11c5d7b3067f
Cris Barbosa <jujucacris@hotmail.com>
2019-10-17 11:40:46 -0300

    scripts convertidos para python3 e pequenas melhorias aplicadas

--- tabela_atividades_nolle.py
+++ tabela_atividades_nolle.py
@@ -24,20 +24,23 @@ def gera_numeros(atividades): # recebe o vetor com todas as atividades
 def descobre_atividades(file): # recebe o nome do arquivo a ser tratado
     f = open(file, "r")
     f = csv.reader(f, delimiter=",") # abertura do arquivo csv como matriz
-
+    s = open(("lista_atividades_" + file), "w")
     atividades = [] # vetor das atividades
     i = 1 # iterador
+    next(f) # pula a primeira linha do arquivo
     for row in f: # itera as linhas do arquivo
-        line = next(f) # pula para a próxima linha (ignorando a primeira)
-        if line[2] in atividades: # verifica se o nome da atividade já existe no vetor
+        #line = next(f) # pula para a próxima linha (ignorando a primeira)
+        if row[2] in atividades: # verifica se o nome da atividade já existe no vetor
             continue # se sim, passa para a próxima iteração
         else:
-            atividades.append(line[2]) # se não, adiciona o nome da atividade no vetor
+            atividades.append(row[2]) # se não, adiciona o nome da atividade no vetor
             i = i + 1
     c = gera_numeros(atividades) # armazena o vetor de representações binárias
     atividades.sort() # ordena as atividades alfabeticamente
-    print(str(len(atividades)) + " atividades") # imprime a quantidade de atividades descobertas
+    print(str(len(atividades)) + " atividades descobertas") # imprime a quantidade de atividades descobertas
     atividades_dict = dict(zip(atividades, c)) # cria um dicionário associando atividades e binários
+    for item in atividades_dict:
+        s.write(item + "\n")
     return atividades_dict # retorna o dicionário criado
 
 # função para formatar as strings dos traces para impressão no arquivo
@@ -62,13 +65,14 @@ def converte(file, a): # recebe o nome do arquivo de entrada e o dicionário de 
     f = csv.reader(f, delimiter=",") # abre o csv como matriz
     p = [] # vetor onde será armazenado cada trace
     r = open("%s-nolle.csv" % file.replace(".csv",""), "w") # abre o arquivo de saída
-    i = 0 # iterador de traces
+    i = 1 # iterador de traces
     max_size = 0 # variável de tamanho máximo de trace
     rows = [] # matriz de saída que será impressa no arquivo
+    next(f)
     for row in f: # para cada linha do arquivo de entrada
-        if i == 0: # se for a primeira iteração, pula para a segunda linha, evitando o cabeçalho
-            i = i + 1
-            continue
+        #if i == 0: # se for a primeira iteração, pula para a segunda linha, evitando o cabeçalho
+        #    i = i + 1
+        #    continue
         if row[0] == str(i): # se o identificador do trace for igual ao que estava sendo tratado
             tipo = row[1] # armazena o tipo de evento, normal ou anomalia
             atividade = a[row[2]] # armazena a atividade realizada em representação binária a partir do dicionário
@@ -89,13 +93,11 @@ def converte(file, a): # recebe o nome do arquivo de entrada e o dicionário de 
     rows = completa(rows, max_size) # chama função que nivela todos os traces
     for row in rows: # para cada linha na matriz de traces
         r.write(formata(row)) # imprime linha formatada no arquivo
-    print(str(max_size) + " eventos") # imprime o tamanho do maior trace
-    print("done!")
+    print(str(max_size) + " eventos no maior trace") # imprime o tamanho do maior trace
+    print("done!\n")
 
 
-
-
-def main(file):
+def binarize(file):
 
     atividades = descobre_atividades(file)
     #pp = pprint.PrettyPrinter()
@@ -109,4 +111,4 @@ if __name__ == '__main__':
         if arg == sys.argv[0]:
             continue
         else:
-            main(arg)
+            binarize(arg)
