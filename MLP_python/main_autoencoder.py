4232073e02a83e1d1ddad6ea37cdac9042191c84
erojask7 <erojask7@gmail.com>
2019-11-11 15:42:39 -0200

    main_autoencoder salvando saidas

--- MLP_python/main_autoencoder.py
+++ MLP_python/main_autoencoder.py
@@ -10,7 +10,7 @@ import numpy as np
 from clase_MLP import cMLP as cMLP
 # leer arquivo do log
 dataset = pd.read_csv("datasets/p2p-0.3-1-nolle.csv")
-Xtest_labels=dataset.iloc[4001:4999,:]
+Xtest_with_labels=dataset.iloc[4001:4999,:]
 dataset=dataset.iloc[:,0:350]
 
 funcao_f='sig'
@@ -23,7 +23,7 @@ Ytr=Xtr;
 #test
 Xtest=np.array(dataset.iloc[4001:4999,0:350]);
 Ytest=Xtest;
-nitmax=100;
+nitmax=10;
 alfa=1;
 no=40;
 
@@ -36,4 +36,13 @@ oMLP = cMLP(funcao_f,funcao_g,no)
 erro = Yout_test-Ytest
 N=len(Yout_test)
 EQMs = np.sum(erro*erro,axis=1)/350
-Xtest_labels[EQMs>0.036]
+
+
+Yd=Xtest_with_labels.iloc[:,350]
+Y=pd.Series(EQMs>0.01)
+Y[EQMs>0.01]='a'
+Y[EQMs<=0.01]='n'
+
+
+Y.to_csv("Y.csv", sep=',', encoding='utf-8', index=False)
+Yd.to_csv("Yd.csv", sep=',', encoding='utf-8',index=False)
