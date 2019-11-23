f7e15db312e55dd32855923b0aa11676761f6df7
Cris Barbosa <jujucacris@hotmail.com>
2019-11-22 23:07:07 -0300

    adicionado arquivo curto com usuários

--- Conversor de JSON/tabela_atividades_nolle_pequenas.py
+++ Conversor de JSON/tabela_atividades_nolle_pequenas.py
@@ -5,24 +5,24 @@ import csv
 
 def corta_traces():
 
-	f = open("p2p-0.3-1-nolle.csv", "r")
+	f = open("p2p-0.3-1-usuarios-nolle.csv", "r")
 	f = csv.reader(f)
 	#max = 14
-	#size = 350
-	chars = 225 # número máximo de caracteres de um trace com até 9 atividades
+	chars = 1476 # número máximo de caracteres de um trace com até 9 atividades
+	trace = 2296
 
 	csv_final = []
 	i = 0
 	for line in f:
-		if "1" in line[225:]:
+		if "1" in line[chars:]:
 			i += 1
 		else:
-			row = line[:255]
-			row.append(line[350])
+			row = line[:chars]
+			row.append(line[trace])
 			csv_final.append(row)
 	print("popped %s items!" % i)
 
-	s = open("p2p-0.3-1-curto.csv", "w+")
+	s = open("p2p-0.3-1-usuarios-curto.csv", "w+")
 	for line in csv_final:
 		s.write(str(line).replace("[","").replace("]","").replace("'","").replace(", ",",")+"\n")
 	s.close()
